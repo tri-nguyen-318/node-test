dd8b4a88d489921fc5b22d15ea118cc7f9e8bc8b
Trí Nguyễn <tri.nguyen@devsamurai.com>
2024-03-26 11:27:25 +0700

    doing

--- index.js
+++ index.js
@@ -5,4 +5,20 @@ import utc from 'dayjs/plugin/utc.js';
 dayjs.extend(timezone);
 dayjs.extend(utc);
 
-console.log('tri')
+function generateIsoTimes() {
+  let isoTimes = [];
+
+  let currentTime = dayjs().startOf('day').format();
+  const endTime = dayjs().endOf('day').format();
+
+  while (currentTime < endTime) {
+    isoTimes.push(currentTime.slice(11, 19));
+    currentTime = dayjs(currentTime).add(30, 'minutes').format();
+  }
+  return isoTimes;
+}
+
+// Example usage
+const isoTimesArray = generateIsoTimes();
+console.log(isoTimesArray);
+console.log('test', 1 + 1)
